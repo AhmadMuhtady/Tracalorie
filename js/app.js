9a942be6f03e141f0d962a6eb7dd6b284dd5c21f
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-08-16 03:28:19 +0300

    Display New Meal & Workout

--- js/app.js
+++ js/app.js
@@ -16,12 +16,14 @@ class CalorieTracker {
 	addMeal(meal) {
 		this.#meals.push(meal);
 		this.#totalCalories += meal.calories;
+		this._displayNewMeal(meal);
 		this._render();
 	}
 
 	addWorkout(workout) {
 		this.#workouts.push(workout);
 		this.#totalCalories -= workout.calories;
+		this._displayNewWorkout(workout);
 		this._render();
 	}
 
@@ -109,6 +111,53 @@ class CalorieTracker {
 		progressEl.style.width = `${width}%`;
 	}
 
+	_displayNewMeal(meal) {
+		const mealsEl = document.getElementById('meal-items');
+		const mealEl = document.createElement('div');
+		mealEl.classList.add('card', 'my-2');
+		mealEl.setAttribute('data-id', meal.id);
+		mealEl.innerHTML = `
+		<div class="card-body">
+			<div class="d-flex align-items-center justify-content-between">
+				<h4 class="mx-1">${meal.name}</h4>
+				<div
+					class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
+				>
+					${meal.calories}
+				</div>
+				<button class="delete btn btn-danger btn-sm mx-2">
+					<i class="fa-solid fa-xmark"></i>
+				</button>
+			</div>
+		</div>
+	`;
+
+		mealsEl.appendChild(mealEl);
+	}
+
+	_displayNewWorkout(workout) {
+		const workoutsEl = document.getElementById('workout-items');
+		const workoutEl = document.createElement('div');
+		workoutEl.classList.add('card', 'my-2');
+		workoutEl.setAttribute('data-id', workout.id);
+		workoutEl.innerHTML = `
+		<div class="card-body">
+			<div class="d-flex align-items-center justify-content-between">
+				<h4 class="mx-1">${workout.name}</h4>
+				<div
+					class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5"
+				>
+					${workout.calories}
+				</div>
+				<button class="delete btn btn-danger btn-sm mx-2">
+					<i class="fa-solid fa-xmark"></i>
+				</button>
+			</div>
+		</div>
+	`;
+
+		workoutsEl.appendChild(workoutEl);
+	}
 	_render() {
 		this._displayCaloriesTotal();
 		this._displayCaloriesConsumed();
